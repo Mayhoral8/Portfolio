037ee50dd69f8d573725967022c132747a445195
Mayowa Ayeni <mayhoral008@gmail.com>
2023-10-31 21:10:16 +0100

    project-data file re-added

---
+++ src/projects/project-data.js
@@ -0,0 +1,53 @@
+
+
+export const projectData = [
+
+    {
+        id: 1,
+        title:'Traxpense',
+        media: '',
+        description: 'lorem ipsum sndamnmndasasmdnas amdnsmad amdnsad',
+        liveLink: '',
+        sourceLink: ''
+    },
+    {
+        id: 2,
+        title:'Techify',
+        media: '',
+        description: '',
+        liveLink: '',
+        sourceLink: ''
+    },
+    {
+        id: 3,
+        title:'Unscramble',
+        media: '',
+        description: 'lorem ipsum sndamnmndasasmdnas amdnsmad amdnsad',
+        liveLink: '',
+        sourceLink: ''
+    },
+    {
+        id: 4,
+        title:'Linklop',
+        media: '',
+        description: '',
+        liveLink: '',
+        sourceLink: ''
+    },
+    {
+        id: 5,
+        title:'Transcert',
+        media: '',
+        description: '',
+        liveLink: '',
+        sourceLink: ''
+    },
+    {
+        id: 6,
+        title:'Auto-Quote-Generator',
+        media: '',
+        description: '',
+        liveLink: '',
+        sourceLink: ''
+    }
+]
